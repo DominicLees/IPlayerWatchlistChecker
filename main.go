da693f0635625c105259c427f0519400d5a68617
Dominic Lees <leesdominic@gmail.com>
2026-07-22 18:32:25 +0100

    Add links to films on results page

--- main.go
+++ main.go
@@ -11,85 +11,90 @@ import (
 
 const port int = 8000
 
-type iplayerFilm struct {
-	title string
-	id    string
+type IPlayerFilm struct {
+	Title string
+	Id    string
 }
 
-func results(w http.ResponseWriter, r *http.Request) {
-	file, _, err := r.FormFile("file")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+func results() http.HandlerFunc {
+	tmpl := template.Must(template.ParseFiles("templates/results.html"))
 
-	reader := csv.NewReader(file)
-	reader.Read() // skip header
-
-	// Read titles from watchlist
-	var watchlist []string
-	for {
-		row, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
+	return func(w http.ResponseWriter, r *http.Request) {
+		file, _, err := r.FormFile("file")
 		if err != nil {
 			panic(err)
 		}
-		watchlist = append(watchlist, row[1])
-	}
+		defer file.Close()
 
-	var foundFilms []iplayerFilm
-	page := 1
-	count := 0
-	for {
-		// Request next page of films
-		resp, err := http.Get(fmt.Sprintf("https://ibl.api.bbci.co.uk/ibl/v1/categories/films/programmes?per_page=200&page=%d", page))
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
+		reader := csv.NewReader(file)
+		reader.Read() // skip header
 
-		// Read response body
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		// Decode JSON
-		var result map[string]interface{}
-		err = json.Unmarshal(body, &result)
-		if err != nil {
-			panic(err)
-		}
-
-		// Reduce to film data
-		data := result["category_programmes"].(map[string]interface{})
-		films := data["elements"].([]interface{})
-
-		// Check for films on watchlist
-		watchSet := make(map[string]struct{}, len(watchlist))
-		for _, w := range watchlist {
-			watchSet[w] = struct{}{}
-		}
-
-		for _, f := range films {
-			filmObj := f.(map[string]interface{})
-			title := filmObj["title"].(string)
-			if _, found := watchSet[title]; !found {
-				continue
+		// Read titles from watchlist
+		var watchlist []string
+		for {
+			row, err := reader.Read()
+			if err == io.EOF {
+				break
 			}
-			id := filmObj["id"].(string)
-			foundFilms = append(foundFilms, iplayerFilm{title: title, id: id})
+			if err != nil {
+				panic(err)
+			}
+			watchlist = append(watchlist, row[1])
 		}
 
-		count += len(films)
-		if count >= int(data["count"].(float64)) {
-			break
+		var foundFilms []IPlayerFilm
+		page := 1
+		count := 0
+		for {
+			// Request next page of films
+			resp, err := http.Get(fmt.Sprintf("https://ibl.api.bbci.co.uk/ibl/v1/categories/films/programmes?per_page=200&page=%d", page))
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			// Read response body
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			// Decode JSON
+			var result map[string]interface{}
+			err = json.Unmarshal(body, &result)
+			if err != nil {
+				panic(err)
+			}
+
+			// Reduce to film data
+			data := result["category_programmes"].(map[string]interface{})
+			films := data["elements"].([]interface{})
+
+			// Check for films on watchlist
+			watchSet := make(map[string]struct{}, len(watchlist))
+			for _, w := range watchlist {
+				watchSet[w] = struct{}{}
+			}
+
+			for _, f := range films {
+				filmObj := f.(map[string]interface{})
+				title := filmObj["title"].(string)
+				if _, found := watchSet[title]; !found {
+					continue
+				}
+				id := filmObj["id"].(string)
+				foundFilms = append(foundFilms, IPlayerFilm{Title: title, Id: id})
+			}
+
+			count += len(films)
+			if count >= int(data["count"].(float64)) {
+				break
+			}
+			page++
 		}
-		page++
+
+		tmpl.Execute(w, foundFilms)
 	}
-	fmt.Fprint(w, foundFilms)
 }
 
 func index() http.HandlerFunc {
@@ -101,7 +106,7 @@ func index() http.HandlerFunc {
 
 func main() {
 	http.HandleFunc("/", index())
-	http.HandleFunc("/results", results)
+	http.HandleFunc("/results", results())
 	fmt.Printf("Server listening on port %d\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
