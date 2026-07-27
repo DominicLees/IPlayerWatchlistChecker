7a0cc41fa815d40e42cd0002c33562d067839894
Dominic Lees <leesdominic@gmail.com>
2026-07-27 16:06:17 +0100

    Move querying bbc api to files.go

--- files.go
+++ files.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
+	"fmt"
 	"io"
 	"mime/multipart"
+	"net/http"
 	"path/filepath"
 	"strings"
 )
+
+type IPlayerFilm struct {
+	Title string
+	Id    string
+}
 
 type ErrNotCSV struct {
 	message string
@@ -39,4 +47,59 @@ func readWatchlistFile(file multipart.File, header *multipart.FileHeader) ([]str
 	}
 
 	return watchlist, nil
+}
+
+func getFilms(watchlist []string) ([]IPlayerFilm, error) {
+	var foundFilms []IPlayerFilm
+	page := 1
+	count := 0
+	for {
+		// Request next page of films
+		resp, err := http.Get(fmt.Sprintf("https://ibl.api.bbci.co.uk/ibl/v1/categories/films/programmes?per_page=200&page=%d", page))
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		// Read response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		// Decode JSON
+		var result map[string]interface{}
+		err = json.Unmarshal(body, &result)
+		if err != nil {
+			return nil, err
+		}
+
+		// Reduce to film data
+		data := result["category_programmes"].(map[string]interface{})
+		films := data["elements"].([]interface{})
+
+		// Check for films on watchlist
+		watchSet := make(map[string]struct{}, len(watchlist))
+		for _, w := range watchlist {
+			watchSet[w] = struct{}{}
+		}
+
+		for _, f := range films {
+			filmObj := f.(map[string]interface{})
+			title := filmObj["title"].(string)
+			if _, found := watchSet[title]; !found {
+				continue
+			}
+			id := filmObj["id"].(string)
+			foundFilms = append(foundFilms, IPlayerFilm{Title: title, Id: id})
+		}
+
+		count += len(films)
+		if count >= int(data["count"].(float64)) {
+			break
+		}
+		page++
+	}
+
+	return foundFilms, nil
 }
--- server.go
+++ server.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
-	"io"
 	"net/http"
 )
-
-type IPlayerFilm struct {
-	Title string
-	Id    string
-}
 
 func results() http.HandlerFunc {
 	tmpl := template.Must(template.ParseFiles("templates/results.html"))
@@ -36,58 +29,11 @@ func results() http.HandlerFunc {
 			return
 		}
 
-		var foundFilms []IPlayerFilm
-		page := 1
-		count := 0
-		for {
-			// Request next page of films
-			resp, err := http.Get(fmt.Sprintf("https://ibl.api.bbci.co.uk/ibl/v1/categories/films/programmes?per_page=200&page=%d", page))
-			if err != nil {
-				http.Redirect(w, r, "/?err=bbc", 301)
-				return
-			}
-			defer resp.Body.Close()
-
-			// Read response body
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				http.Redirect(w, r, "/?err=bbc", 301)
-				return
-			}
-
-			// Decode JSON
-			var result map[string]interface{}
-			err = json.Unmarshal(body, &result)
-			if err != nil {
-				http.Redirect(w, r, "/?err=bbc", 301)
-				return
-			}
-
-			// Reduce to film data
-			data := result["category_programmes"].(map[string]interface{})
-			films := data["elements"].([]interface{})
-
-			// Check for films on watchlist
-			watchSet := make(map[string]struct{}, len(watchlist))
-			for _, w := range watchlist {
-				watchSet[w] = struct{}{}
-			}
-
-			for _, f := range films {
-				filmObj := f.(map[string]interface{})
-				title := filmObj["title"].(string)
-				if _, found := watchSet[title]; !found {
-					continue
-				}
-				id := filmObj["id"].(string)
-				foundFilms = append(foundFilms, IPlayerFilm{Title: title, Id: id})
-			}
-
-			count += len(films)
-			if count >= int(data["count"].(float64)) {
-				break
-			}
-			page++
+		foundFilms, err := getFilms(watchlist)
+		if err != nil {
+			http.Redirect(w, r, "/?err=bbc", 301)
+			fmt.Println(err)
+			return
 		}
 
 		tmpl.Execute(w, foundFilms)
