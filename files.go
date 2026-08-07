e315864d8e4d9d203c732dae31008fcf8d4cf5fb
Dominic Lees <leesdominic@gmail.com>
2026-08-07 11:40:10 +0100

    Add additional error checking to readWatchlistFile()

--- files.go
+++ files.go
@@ -8,22 +8,37 @@ import (
 	"strings"
 )
 
-type ErrNotCSV struct {
+type ErrFileNotCSV struct {
 	message string
 }
 
-func (e *ErrNotCSV) Error() string {
+func (e *ErrFileNotCSV) Error() string {
+	return e.message
+}
+
+type ErrFileNotListOfFilms struct {
+	message string
+}
+
+func (e *ErrFileNotListOfFilms) Error() string {
 	return e.message
 }
 
 func readWatchlistFile(file multipart.File, filename string) ([]string, error) {
 	// Check csv file was passed
 	if strings.ToLower(filepath.Ext(filename)) != ".csv" {
-		return nil, &ErrNotCSV{"File does not have .csv extension"}
+		return nil, &ErrFileNotCSV{"File does not have .csv extension"}
 	}
 
 	reader := csv.NewReader(file)
-	reader.Read() // Skip header
+	header, err := reader.Read()
+	if err != nil {
+		return nil, err
+	}
+	// Check header layout matches that of a CSV file provided by Letterboxd
+	if len(header) != 4 || header[3] != "Letterboxd URI" {
+		return nil, &ErrFileNotListOfFilms{"CSV contents not formatted as a list of films"}
+	}
 
 	// Read film titles from watchlist file
 	var watchlist []string
